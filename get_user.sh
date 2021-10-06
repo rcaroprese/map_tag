e34c9ad58bc9953383bf38253ed236a4957ad205
Rafael <rafacaropre@gmail.com>
2021-10-06 09:49:26 +0200

    Modified action

--- get_user.sh
+++ get_user.sh
@@ -1,9 +1,11 @@
 shopt -s extglob
 tag=$1
 
-case $tag in
+user=$(case $tag in
     dev* ) echo 1;;
     tst* ) echo 2;;
     acc* ) echo 3;;
     prd* ) echo 4;;
-esac
+esac)
+
+echo "::set-output name=user-number::$(user)"
