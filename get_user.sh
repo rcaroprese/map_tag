d654bf7c64f6ead2d74ce40335fec09c26ff26dd
Rafael <rafacaropre@gmail.com>
2021-10-06 13:37:42 +0200

    Modified script

--- get_user.sh
+++ get_user.sh
@@ -2,10 +2,10 @@ shopt -s extglob
 tag=$1
 
 user=$(case $tag in
-    dev* ) echo 1;;
-    tst* ) echo 2;;
-    acc* ) echo 3;;
-    prd* ) echo 4;;
+    dev* ) echo $DEV_ENV;;
+    tst* ) echo $TST_ENV;;
+    acc* ) echo $ACC_ENV;;
+    prd* ) echo $PRD_ENV;;
 esac)
 
 echo ::set-output name=user-number::"$user"
